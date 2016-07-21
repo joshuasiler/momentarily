a13453e6d14779040df1f1da9b4e88ade76c8440
Joshua Siler <joshua.siler@gmail.com>
2016-07-20 18:51:43 -0700

    readme update

--- lib/momentarily.rb
+++ lib/momentarily.rb
@@ -11,7 +11,7 @@ require 'active_record'
 require 'active_support/core_ext/module'
 
 module Momentarily
-	mattr_accessor :timeout, :debug
+	mattr_accessor :timeout, :debug, :inline
 	@@timeout = 60
 	@@debug = false
 	@@inline = false
